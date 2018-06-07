4c78b4aab54a12c2299fc0b715d65f81334716f8
Andrew Launder <andrew.m.launder@gmail.com>
2018-06-07 15:17:45 -0700

    Fixed date.

--- latticegas/latticegas.cpp
+++ latticegas/latticegas.cpp
@@ -1,7 +1,7 @@
  /* Two-dimensional square lattice gas model
     by Andrew M. Launder
     
-    Last updated 06.05.2018.
+    Last updated 06.07.2018.
     
     See README for proper code usage. */
 
